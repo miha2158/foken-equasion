e19bbd7eab78ab407827bd64a716febdc1f17372
miha2158 <miha.vanya@gmail.com>
2017-10-05 19:40:03 +0500

    готово

--- foken equasion/Program.cs
+++ foken equasion/Program.cs
@@ -12,7 +12,7 @@ namespace foken_equasion
             t = new Stopwatch();
             t.Start();
 
-            long result = 0;
+            long result = (long)(Math.Pow(-1, n) * -8 + Math.Pow(-2, n)* 17.0 / 3 + 2 * n  +7.0 / 3);
 
             t.Stop();
             return result;
@@ -22,22 +22,23 @@ namespace foken_equasion
         {
             t = new Stopwatch();
             t.Start();
-            long x0 = 0, x1 = 1, x2;
+            long x0 = 0, x1 = 1;
 
-            if (n == 0)
-                return x0;
-            if (n == 1)
-                return x1;
-
-            n -= 2;
-
-            do
+            switch (n)
             {
-                x2 = 12 * (n+2) - 3 * x1 - 2 * x0;
+                case 0:
+                    return x0;
+                case 1:
+                    return x1;
+            }
+
+            long x2 = 0;
+            for (int i = 2; i <= n; i++)
+            {
+                x2 = 12 * i - 3 * x1 - 2 * x0;
                 x0 = x1;
                 x1 = x2;
             }
-            while (n-- > 0);
             t.Stop();
 
             return x2;
@@ -45,13 +46,17 @@ namespace foken_equasion
 
         static long recEq(int n)
         {
-            if (n == 0)
-                return 0;
-            if (n == 1)
-                return 1;
-
-            return 12*n - 3 * recEq(n - 1) - 2 * recEq(n - 2);
+            switch (n)
+            {
+                case 0:
+                    return 0;
+                case 1:
+                    return 1;
+                default:
+                    return 12 * n - 3 * recEq(n - 1) - 2 * recEq(n - 2);
+            }
         }
+
         static long recEq(int n, out Stopwatch t)
         {
             t = new Stopwatch();
@@ -68,17 +73,18 @@ namespace foken_equasion
             int num;
             do
             {
+                WriteLine("Введите номер вычисляемого элемента");
                 while (!int.TryParse(n = ReadLine(), out num))
                     WriteLine("Число!");
                 if (n.Contains("-"))
                     break;
                 Clear();
                 WriteLine(n);
-                
-                /*WriteLine("Формула");
-                WriteLine("{0}. {2}t = {1}ms");
+
+                WriteLine("Формула");
+                WriteLine("{0}. {2}t = {1}ms", dirEq(num, out t), t.ElapsedMilliseconds, t.ElapsedTicks);
                 t = null;
-                GC.Collect();*/
+                GC.Collect();
                 WriteLine("Иттерация");
                 WriteLine("{0}. {2}t = {1}ms", ittEq(num, out t), t.ElapsedMilliseconds, t.ElapsedTicks);
                 t = null;
